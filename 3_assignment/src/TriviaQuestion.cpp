ed0b158adf1bdb12f4edb23b27e4f44e33fb76ce
JakeLandowski <jakelandowski@gmail.com>
2018-01-19 09:48:28 -0800

    finished split function

--- 3_assignment/src/TriviaQuestion.cpp
+++ 3_assignment/src/TriviaQuestion.cpp
@@ -1,9 +1,13 @@
+#include <time.h>
+#include <stdlib.h>
+
 #include "TriviaQuestion.h"
 
 using namespace std;
 
 TriviaQuestion::TriviaQuestion(string data)
 {
+    srand(time(0));
     setup(data);  
 }
 
@@ -11,50 +15,26 @@ void TriviaQuestion::setup(string data)
 {
     if(data.empty()) return;
 
-    string delimiter = " :: ";
-    size_t start_pos = 0;
-    size_t found_pos;
+    vector<string> components = split(data, " :: ", 5);
 
-    if((found_pos = data.find(delimiter, start_pos)) != string::npos)
-    {
-            //  set question equal to string up til first delimiter
-        question  = data.substr(start_pos, found_pos - start_pos);
+    question   = components[0];
+    answers[0] = components[1];
+    answers[1] = components[2];
+    answers[2] = components[3];
+    answers[3] = components[4];
 
-            //  skip to the position AFTER this delimiter
-        start_pos = found_pos + delimiter.length(); 
-    }
-
-    int index = 0;
-
-    while((found_pos = data.find(delimiter, start_pos)) != string::npos)
-    {
-        if(index > 3) 
-        {
-            cerr << "Too many answer arguments given to TriviaQuestion setup()" << endl;
-            break;
-        }
-
-            //  set answer equal to string up til first delimiter
-        answers[index] = data.substr(start_pos, found_pos - start_pos);
-        
-            //  skip to the position AFTER this delimiter
-        start_pos = found_pos + delimiter.length();
-        index++;
-    }
-
-    answers[3] = data.substr(start_pos);
-
-    cout << "\"" << question << "\""  << endl
-         << "\""<< answers[0] << "\"" << endl
-         << "\""<< answers[1] << "\"" << endl
-         << "\""<< answers[2] << "\"" << endl
-         << "\""<< answers[3] << "\"" << endl;
+    cout << "\"" << question   << "\"" << endl
+         << "\"" << answers[0] << "\"" << endl
+         << "\"" << answers[1] << "\"" << endl
+         << "\"" << answers[2] << "\"" << endl
+         << "\"" << answers[3] << "\"" << endl;
 }
 
 int TriviaQuestion::askQuestion()
 {
     if(question.empty()) cerr << "Question not setup yet." << endl;
 
+    rand();
     // figure out random order
     // figure out correct answer number
     // ask question
@@ -63,4 +43,29 @@ int TriviaQuestion::askQuestion()
     // etc
 
     return 0; 
+}
+
+vector<string> const TriviaQuestion::split(const string &splitting, 
+                                           const string &delimiter,
+                                           int estimatedCapacity)
+{
+    vector<string> tokens;
+    tokens.reserve(estimatedCapacity);
+    
+    size_t start_pos = 0;
+    size_t found_pos;
+
+    while((found_pos = splitting.find(delimiter, start_pos)) != string::npos)
+    {
+            //  push string up til first delimiter
+        tokens.push_back(splitting.substr(start_pos, found_pos - start_pos));
+        
+            //  skip to the position AFTER this delimiter
+        start_pos = found_pos + delimiter.length();
+    }
+
+        //  fence post last token
+    tokens.push_back(splitting.substr(start_pos));
+
+    return tokens;
 }
--- 3_assignment/src/TriviaQuestion.h
+++ 3_assignment/src/TriviaQuestion.h
@@ -3,17 +3,22 @@
 
 #include <string> // in case iostream doesn't include
 #include <iostream>
+#include <vector>
 
 class TriviaQuestion
 {
-    private:
-        std::string question;
-        std::string answers[4];
-
     public:
         TriviaQuestion(std::string data = "");
         void setup(std::string data); 
         int askQuestion();
+
+    private:
+        std::string question;
+        std::string answers[4];
+
+        static std::vector<std::string> const split(const std::string &splitting, 
+                                                    const std::string &delimiter,
+                                                    int estimatedCapacity = 10);
 };
 
 #endif
