55b211b89e88d35ff27f918e216f4d1f5b39510a
JakeLandowski <jakelandowski@gmail.com>
2018-01-19 11:38:39 -0800

    Extracted split function and made a utilities library, yay

---
+++ 3_assignment/src/Utilities.cpp
@@ -0,0 +1,30 @@
+#include "Utilities.h"
+
+using namespace std;
+
+
+vector<string> Utilities::split(const string &splitting, 
+                                const string &delimiter,
+                                int estimatedCapacity)
+{
+    vector<string> tokens;
+    tokens.reserve(estimatedCapacity);
+    
+    size_t start_pos = 0;
+    size_t found_pos;
+
+        //  find position of first delimiter, if found continue
+    while((found_pos = splitting.find(delimiter, start_pos)) != string::npos)
+    {
+            //  push string up til first delimiter
+        tokens.push_back(splitting.substr(start_pos, found_pos - start_pos));
+        
+            //  skip to the position AFTER this delimiter
+        start_pos = found_pos + delimiter.length();
+    }
+
+        //  fence post last token
+    tokens.push_back(splitting.substr(start_pos));
+
+    return tokens;
+}
---
+++ 3_assignment/src/Utilities.h
@@ -0,0 +1,21 @@
+#ifndef H_Utilities_H
+#define H_Utilities_H
+
+#include <string> // in case iostream doesn't include
+#include <iostream>
+#include <vector>
+
+namespace Utilities
+{
+    /**
+     *  Java-like split function. Takes a string, and a delimiter,
+     *  then returns a vector with those string chunks. Third 
+     *  optional parameter to specify initial vector capacity
+     *  if you want to estimate the size you'll need for efficiency.
+    */
+    std::vector<std::string> split(const std::string &splitting, 
+                                   const std::string &delimiter,
+                                   int estimatedCapacity = 10);
+}
+
+#endif
