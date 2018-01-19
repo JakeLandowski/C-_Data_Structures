d79e7e9da138417846140b6342431c594246408f
JakeLandowski <jakelandowski@gmail.com>
2018-01-19 08:54:59 -0800

    Finished parsing question in setup

---
+++ 3_assignment/src/TriviaMain.cpp
@@ -0,0 +1,21 @@
+/**
+ *  Jacob Landowski, CS132, Winter 2667, Section A
+ *  Assignment #2, 1/10/18
+ * 
+ *  This program...
+ */
+
+
+#include <string> // in case iostream doesn't include
+#include <iostream>
+
+#include "TriviaQuestion.h"
+
+using namespace std;
+
+int main()
+{
+    TriviaQuestion question("question here :: answer 1 here :: answer 2 here :: answer 3 here :: answer 4 here");
+
+    return 0;
+}
--- 3_assignment/src/TriviaQuestion.cpp
+++ 3_assignment/src/TriviaQuestion.cpp
@@ -11,11 +11,56 @@ void TriviaQuestion::setup(string data)
 {
     if(data.empty()) return;
 
+    string delimiter = " :: ";
+    size_t start_pos = 0;
+    size_t found_pos;
 
-    // parse question
+    if((found_pos = data.find(delimiter, start_pos)) != string::npos)
+    {
+            //  set question equal to string up til first delimiter
+        question  = data.substr(start_pos, found_pos - start_pos);
+
+            //  skip to the position AFTER this delimiter
+        start_pos = found_pos + delimiter.length(); 
+    }
+
+    int index = 0;
+
+    while((found_pos = data.find(delimiter, start_pos)) != string::npos)
+    {
+        if(index > 3) 
+        {
+            cerr << "Too many answer arguments given to TriviaQuestion setup()" << endl;
+            break;
+        }
+
+            //  set answer equal to string up til first delimiter
+        answers[index] = data.substr(start_pos, found_pos - start_pos);
+        
+            //  skip to the position AFTER this delimiter
+        start_pos = found_pos + delimiter.length();
+        index++;
+    }
+
+    answers[3] = data.substr(start_pos);
+
+    cout << "\"" << question << "\""  << endl
+         << "\""<< answers[0] << "\"" << endl
+         << "\""<< answers[1] << "\"" << endl
+         << "\""<< answers[2] << "\"" << endl
+         << "\""<< answers[3] << "\"" << endl;
 }
 
 int TriviaQuestion::askQuestion()
 {
-    if(question.empty()) cerr << "Question not setup yet." << endl; 
+    if(question.empty()) cerr << "Question not setup yet." << endl;
+
+    // figure out random order
+    // figure out correct answer number
+    // ask question
+    // get input
+    // check answer
+    // etc
+
+    return 0; 
 }
